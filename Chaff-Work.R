52acf1629c7edb0ab8e4b23a8f9c81e4d213ee8a
Lou Mamalis <lcm566@york.ac.uk>
2020-10-22 16:01:16 +0100

    i have made a change

---
+++ Chaff-Work.R
@@ -0,0 +1,22 @@
+chaff <- read.table('chaff.txt', header = T)
+
+chaff2<- chaff%>% 
+  pivot_longer(names_to = "sex", 
+               values_to = "mass",
+               cols = everything())
+
+summary(file)
+library('tidyverse')
+
+chaff2
+summary(chaff2)
+
+chaff2 %>% group_by(sex) %>%
+  summarise(mean = mean(mass),
+            n = length(mass),
+            sdev = sd(mass),
+            median = median(mass))
+
+t.test(data = chaff2, #two sample t.test significant difference between male and female mass 
+       mass ~ sex,
+       var.equal = T)
